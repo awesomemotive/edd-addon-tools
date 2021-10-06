1bbd2d76d9bb7a1901de369217b4023fe61f04eb
Ashley Gibson <ashley@nosegraze.com>
2021-10-06 13:18:56 +0100

    Show errors inline on plugin row.

--- src/v1/ExtensionLoader.php
+++ src/v1/ExtensionLoader.php
@@ -20,6 +20,11 @@ class ExtensionLoader {
 	private $pluginFile;
 
 	/**
+	 * @var string Plugin basename.
+	 */
+	private $pluginBasename;
+
+	/**
 	 * @var \Closure|\Callable|mixed Function/method to execute if all requirements are met.
 	 */
 	private $callback;
@@ -32,9 +37,9 @@ class ExtensionLoader {
 	/**
 	 * Constructor.
 	 *
-	 * @param string $pluginFile       Path to the plugin file ( __FILE__ from main plugin file.)
+	 * @param string $pluginFile   Path to the plugin file ( __FILE__ from main plugin file.)
 	 * @param \Closure|\Callable|mixed Callback to execute if requirements are met.
-	 * @param array $requirements      Requirements to pass to the checker class.
+	 * @param array  $requirements Requirements to pass to the checker class.
 	 *
 	 * @throws \InvalidArgumentException
 	 */
@@ -44,6 +49,7 @@ class ExtensionLoader {
 		}
 
 		$this->pluginFile          = $pluginFile;
+		$this->pluginBasename      = plugin_basename( $this->pluginFile );
 		$this->callback            = $callback;
 		$this->requirementsChecker = new RequirementsChecker( $requirements );
 	}
@@ -63,41 +69,85 @@ class ExtensionLoader {
 		if ( $this->requirementsChecker->met() ) {
 			call_user_func( $this->callback );
 		} else {
-			add_action( 'admin_notices', array( $this, 'printLoadingErrors' ) );
+			add_action( 'admin_head', array( $this, 'loadingErrorsCss' ) );
+			add_action( "after_plugin_row_{$this->pluginBasename}", array( $this, 'addErrorsToPluginRow' ) );
 		}
 	}
 
 	/**
-	 * Displays errors about unmet requirements.
+	 * Adds CSS to the admin head for our "plugin not fully loaded" UI.
+	 *
+	 * @since 1.0
 	 */
-	public function printLoadingErrors() {
-		$pluginData = get_plugin_data( $this->pluginFile );
+	public function loadingErrorsCss() {
+		$className = sanitize_html_class( $this->pluginBasename );
 		?>
-		<div class="notice notice-error">
-			<p>
+		<style id="<?php echo esc_attr( $className ); ?>">
+            .plugins tr[data-plugin="<?php echo esc_html( $this->pluginBasename ); ?>"] th,
+            .plugins tr[data-plugin="<?php echo esc_html( $this->pluginBasename ); ?>"] td,
+            .plugins .<?php echo esc_html( $className ); ?>-row th,
+            .plugins .<?php echo esc_html( $className ); ?>-row td {
+                background: #fff5f5;
+            }
+
+            .plugins tr[data-plugin="<?php echo esc_html( $this->pluginBasename ); ?>"] th {
+                box-shadow: none;
+            }
+
+            .plugins .<?php echo esc_html( $className ); ?>-row th span {
+                margin-left: 6px;
+                color: #dc3232;
+            }
+
+            .plugins tr[data-plugin="<?php echo esc_html( $this->pluginBasename ); ?>"] th,
+            .plugins .<?php echo esc_html( $className ); ?>-row th.check-column {
+                border-left: 4px solid #dc3232 !important;
+            }
+
+            .plugins .<?php echo esc_html( $className ); ?>-row .column-description p {
+                margin: 0;
+                padding: 0;
+            }
+
+            .plugins .<?php echo esc_html( $className ); ?>-row .column-description p:not(:last-of-type) {
+                margin-bottom: 8px;
+            }
+		</style>
+		<?php
+	}
+
+	/**
+	 * Displays errors about unmet requirements in this plugin's row.
+	 *
+	 * @since 1.0
+	 */
+	public function addErrorsToPluginRow() {
+		$colspan = function_exists( 'wp_is_auto_update_enabled_for_type' ) && wp_is_auto_update_enabled_for_type( 'plugin' ) ? 2 : 1;
+		?>
+		<tr class="active <?php echo esc_attr( sanitize_html_class( $this->pluginBasename ) ); ?>-row">
+			<th class="check-column">
+				<span class="dashicons dashicons-warning"></span>
+			</th>
+			<td class="column-primary">
+				<?php esc_html_e( 'This plugin is not fully active.', 'easy-digital-downloads' ) ?>
+			</td>
+			<td class="column-description" colspan="<?php echo esc_attr( $colspan ); ?>">
 				<?php
-				printf(
-				/* Translators: %1$s name of the plugin */
-					esc_html__( '%1$s is not fully active. The following requirements have not been met:', 'easy-digital-downloads' ),
-					'<strong>' . esc_html( $pluginData['Name'] ) . '</strong>'
-				);
+				foreach ( $this->requirementsChecker->getErrors()->get_error_messages() as $message ) {
+					echo wpautop( wp_kses_post( $message ) );
+				}
 				?>
-			</p>
-			<?php
-			foreach ( $this->requirementsChecker->getErrors()->get_error_messages() as $message ) {
-				echo wpautop( wp_kses_post( $message ) );
-			}
-			?>
-		</div>
+			</td>
+		</tr>
 		<?php
 	}
 
 	/**
 	 * Loads the plugin if requirements are met.
 	 *
-	 * @param string $pluginFile       Path to the plugin file ( __FILE__ from main plugin file.)
+	 * @param string $pluginFile   Path to the plugin file ( __FILE__ from main plugin file.)
 	 * @param \Closure|\Callable|mixed Callback to execute if requirements are met.
-	 * @param array  $requirements     Requirements to launch the plugin.
+	 * @param array  $requirements Requirements to launch the plugin.
 	 */
 	public static function loadOrQuit( $pluginFile, $callback, $requirements ) {
 		$loader = new self( $pluginFile, $callback, $requirements );
